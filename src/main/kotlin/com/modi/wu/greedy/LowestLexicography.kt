fc0bdc411bf57bdb36381cde2f4ec2f750e59c25
modi.wu <wudongsheng7@gmail.com>
2023-10-31 12:49:33 +0800

    feat(greedy): Lowest lexicography greedy algorithm

--- src/main/kotlin/com/modi/wu/greedy/LowestLexicography.kt
+++ src/main/kotlin/com/modi/wu/greedy/LowestLexicography.kt
@@ -20,8 +20,10 @@ fun Array<String>.lowestString():String{
     val all= mutableListOf<String>()
     val use= hashSetOf<Int>()
 
-    process(this,all,use,"")
+    all.take(1)
 
+//    process(this,all,use,"")
+    process1(this.toList(),all,"")
     //存放字符串
     var lowest= all[0]
     for (i in 1 until all.size){
@@ -34,6 +36,16 @@ fun Array<String>.lowestString():String{
 
 }
 
+fun process1(remaining:List<String>, all: MutableList<String>, path: String){
+    if (remaining.isEmpty()){
+        all.add(path)
+    }else{
+        for (i in remaining.indices){
+            process1(remaining.take(i)+remaining.drop(i+1),all,path+remaining[i])
+        }
+    }
+}
+
 fun process(strs:Array<String>,all:MutableList<String>,use:HashSet<Int>,path:String){
 
     if (use.size==strs.size){
@@ -42,14 +54,43 @@ fun process(strs:Array<String>,all:MutableList<String>,use:HashSet<Int>,path:Str
         for (i in strs.indices){
             if (!use.contains(i)){
                 use.add(i)
+                println("process前--i----$i----use---${use.toList()}           path---$path   all--$all\n" )
+
                 process(strs, all, use, path+strs[i])
+                println("i----$i----use---${use.toList()}           path---$path   all--$all\n" )
                 use.remove(i)
+                println("remove--- i----$i----use---${use.toList()}           path---$path   all--$all\n" )
+
+
+
             }
         }
 
     }
 
 }
+
+
+
+/****************************贪心算法************************************/
+
+/**
+ *  例如[ac,db,ab,ck,kz]
+ *  拼接结果中字典序最小的是：abacckdbkz
+ *
+ *
+ * */
+fun Array<String>.greedyLexicographicConcatenation():String{
+    this.sortWith { o1, o2 ->
+        (o1 + o2).compareTo(o2 + o1)
+    }
+    val result=StringBuilder()
+    for (str in this){
+        result.append(str)
+    }
+    return result.toString()
+}
+
 
 fun main() {
     val array=arrayOf("ac","db","ab","ck","kz")
--- src/main/kotlin/com/modi/wu/greedy/README.md
+++ src/main/kotlin/com/modi/wu/greedy/README.md
@@ -1,1 +1,4 @@
 ## 贪心算法（greedy algorithm）
+
+[字符拼接最小字典序](LowestLexicography.kt)
+* 
