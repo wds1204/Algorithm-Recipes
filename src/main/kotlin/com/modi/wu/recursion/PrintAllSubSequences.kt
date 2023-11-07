0ebcf37257f8a478d006e41fb30ebf09613f15c2
modi.wu <784293338@qq.com>
2023-11-07 23:37:19 +0800

    perf(Recursion): Determine whether the string is a subsequence

--- src/main/kotlin/com/modi/wu/recursion/PrintAllSubSequences.kt
+++ src/main/kotlin/com/modi/wu/recursion/PrintAllSubSequences.kt
@@ -27,8 +27,35 @@ fun printSubsequences(s: String, current: String = "", index: Int = 0) {
     printSubsequences(s, current, index + 1)
 }
 
+/**
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ *
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。
+ * （例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ */
+fun isSubsequence(s: String, t: String): Boolean {
+
+    val n = s.length
+    val m = t.length
+    var i = 0
+    var j = 0
+    while (j < m) {
+        if (i < n && s[i] == t[j]) {
+            i++
+            j++
+        } else {
+            j++
+        }
+    }
+    return i == n
+
+
+}
+
 fun main() {
     val inputString = "abc"
     println("字符串 '$inputString' 的全部子序列:")
     printSubsequences(inputString)
+
+    println(isSubsequence("ac", "abcd"))
 }
--- src/main/kotlin/com/modi/wu/recursion/README.md
+++ src/main/kotlin/com/modi/wu/recursion/README.md
@@ -4,4 +4,5 @@
 * [逆序栈](ReverseStack.kt)
 * [袋子中能装下多大价值的物品](Knapsack.kt)
 * [打印字符串的全部子序列](PrintAllSubSequences.kt)
+* [打印字符串的全部排列](PrintAllPermutations.kt)
 
